22eeecc41a26dd76a1a4da121ad19a09c2577f22
anandhu2205 <svananthu2@gmail.com>
2021-11-30 12:57:35 +0530

    revamped ffmpeg and tcp_server

--- Video_Transcoding_Tool/index.js
+++ Video_Transcoding_Tool/index.js
@@ -4,7 +4,7 @@ const server = require('http').createServer(app);
 const cors = require('cors');
 app.use(cors());
 const net = require('net');
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const config = require(path.join(__dirname, './settings/config'));
 const port = config.transcodingPort;
@@ -14,40 +14,75 @@ const io = require('socket.io')(server, {
     orgin: '*',
   },
 });
-let numbersOfClients = [];
+
 app.get('/', (req, res) => {
   res.json(`Transcoding Tool`);
 });
 app.get('/media/:stream/:quality', (req, res) => {
   res.sendFile(`/home/node/media/${req.params.stream}/${req.params.quality}`);
+  // res.sendFile(`/home/user/Desktop/storage/${req.params.stream}/${req.params.quality}`);
 });
 server.listen(port, () => {
   console.log('server running on ', port);
 });
+
+let streamTotal = [];
 const mediaPath = path.join('/home/node/media/');
+//const mediaPath = path.join('/home/user/Desktop/storage/');
 const streamFiles = async () => {
-  fs.readdir(mediaPath, (err, folder) => {
+  try {
+    await fs.ensureDir(mediaPath);
+    const files = await fs.readdir(mediaPath);
+    for (const file of files) {
+      const pathExists = await fs.pathExists(
+        `${mediaPath}/${file}/master.m3u8`,
+      );
+
+      if (pathExists) {
+        const stats = await fs.stat(`${mediaPath}/${file}/master.m3u8`);
+
+        // console.log(file,pathExists,stats.birthtime,'check')
+
+        streamTotal.push({
+          name: file,
+          time: stats.birthtime,
+        });
+        // console.log(streamTotal)
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  /* fs.readdir(mediaPath, (err, folder) => {
     if (err) {
       return console.log('no media folder is found', err);
     }
-    folder.forEach((stream) => {
-      if (numbersOfClients.includes(stream) === true) {
+    console.log(folder)
+/*     folder.forEach((stream) => {
+      if (streamTotal.includes(stream) === true) {
         // console.log('stream already exists')
       } else {
         let masterExists = fs.existsSync(`${mediaPath}/${stream}/master.m3u8`);
         if (masterExists) {
-          numbersOfClients.push(stream);
+          streamTotal.push(stream);
         }
       }
     });
-  });
+  }); */
 };
-io.on('connection', (socket) => {
-  streamFiles().then(() => {
-    socket.emit('playerLoaded', numbersOfClients);
-  });
+io.on('connection', async (socket) => {
+  streamTotal = [];
+
+  await streamFiles();
+  console.log(streamTotal, 'after');
+  socket.emit('playerLoaded', streamTotal);
+  /*   streamFiles().then(() => {
+    socket.emit('playerLoaded', streamTotal);
+  }); */
 });
-//  io.sockets.emit('stream', numbersOfClients);
+
+//  io.sockets.emit('stream', streamTotal);
 const tcpServerUrl = new URL(config.tcp_address);
 var ffmpegConnection = net.connect(
   {
@@ -62,41 +97,79 @@ var ffmpegConnection = net.connect(
     });
   },
 );
-ffmpegConnection.on('data', (data) => {
-  // numbersOfClients.push(data.toString());
+ffmpegConnection.on('data', async (data) => {
+  // streamTotal.push(data.toString());
 
   console.log(data.toString(), 'check');
   streamId = data.toString();
   let streamName = streamId;
   let id = parseInt(streamId.match(/\d+/)[0]);
-  const increaseNumber = () => {
+
+  /*  const checkStreamName = (streamName) => {
+    if (streamTotal.includes(streamName)) {
+      increaseNumber();
+    } else {
+      ffmpeg(streamName);
+      socketEmit(streamName);
+    }
+  }; */
+  const CheckExisting = () => {
+    return streamTotal.find((id, index) => {
+      if (id.name === streamName) {
+        return true;
+      }
+    });
+  };
+
+  const increaseStreamId = async () => {
     id++;
     streamName = streamId.replace(/\d+/, id);
-    if (!numbersOfClients.includes(streamName)) {
-      ffmpeg(streamName);
-      socketEmit(streamName);
-    } else {
-      increaseNumber();
-    }
-  };
-  const checkStreamName = (streamName) => {
-    if (numbersOfClients.includes(streamName)) {
-      increaseNumber();
+    console.log(CheckExisting(), 'check1');
+    if (CheckExisting()) {
+      increaseStreamId();
     } else {
       ffmpeg(streamName);
       socketEmit(streamName);
     }
   };
-  checkStreamName(streamName);
-});
 
+  let folders = await fs.readdir(mediaPath);
+
+  console.log(folders.length);
+
+  if (folders.length !== 0) {
+    if (CheckExisting()) {
+      increaseStreamId();
+    } else {
+      ffmpeg(streamName);
+      socketEmit(streamName);
+    }
+  } else {
+    ffmpeg(streamName);
+    socketEmit(streamName);
+  }
+});
 const socketEmit = (streamName) => {
-  const intervalObj = setInterval(function () {
+  const intervalObj = setInterval(async () => {
     const file = `/home/node/media/${streamName}/master.m3u8`;
-    const fileExists = fs.existsSync(file);
-    if (fileExists) {
-      io.sockets.emit('onStreamAdd', streamName);
-      clearInterval(intervalObj);
+    // const file = `/home/user/Desktop/storage/${streamName}/master.m3u8`;
+    try {
+      const fileExists = await fs.pathExists(file);
+      console.log(fileExists, 'check fileExists');
+      const stats = await fs.stat(`${file}`);
+
+      // let streamCreated;
+      if (fileExists) {
+        console.log('emit times');
+        // streamTotal.push()
+        io.sockets.emit('onStreamAdd', {
+          name: streamName,
+          time: stats.birthtime,
+        });
+        clearInterval(intervalObj);
+      }
+    } catch (e) {
+      console.log(e, 'error');
     }
   }, 2000);
 };
