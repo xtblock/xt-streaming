aded50e646e7bc36f2ab7e5698dbf6dad10de661
sreejesh79 <sreejesh@kriyatma.com>
2021-12-21 13:34:06 +0530

    tcp/phase2

--- tcp_server/sub.js
+++ tcp_server/sub.js
@@ -1,76 +1,169 @@
-const net = require('net')
-const config = require('./settings/tcpConfiguration.json')
+const net = require('net');
+const events = require('events');
+const Emmiter = new events.EventEmitter();
 
-const server = net.createServer()
-const mainTcpSockets =[];
-const transcodingSocket=[];
+const mainConnection = net.connect(
+      {
+            port: 9000,
+            host: '3.110.188.150',
+      },
+      () => {
+            console.log('connected to TCP Server');
+            mainConnection.once('end', () => {
+                  console.log('disconnected from Main Server');
+                  //process.exit(0);
+            });
+      },
+);
+mainConnection.on('data', (data) => {
+      console.log(data.toString());
+      if (data.toString().includes('start:')) {
+            createConnection();
+            for (const sockets of commSockets) {
+                  sockets.write(data.toString().split(":")[1]);
+            }
 
-
-
-let ffmpegObj = []
-
-
-server.on ('connection', (socket) => {
-
-if(!(mainTcpSockets.includes(socket.remoteAddress))){
-      ffmpegObj.push(socket)
-}else{
-      // mainTcpSockets.push(socket)
-}
-
-socket.on('data', (data) => {
-if(data.toString().includes('address:')){
-      mainTcpSockets.push(socket)
-      console.log("data",data.toString().split('address:')[1])
-      for(const transcodingSockets of transcodingSocket){
-            transcodingSockets.write(data)
       }
+});
 
-}else{
-      console.log(ffmpegObj.length)
-            
-                  ffmpegObj[0].write(data)
-                    
-      }
+const createConnection = () => {
+      const client1 = net.connect({
+            port: 8000,
+            host: '3.110.188.150',
+      });
+      const client2 = net.connect({
+            port: 9070,
+            host: '127.0.0.1'
+      }, () => { });
+      client1.on('data',data=>client2.write(data))
 
-})
-socket.on('close', () => {
-      console.log('client disconnected')})
-})
-server.listen(config.ports.SubTcpMain, '0.0.0.0', () => {
-      console.log('server is running on port', config.ports.SubTcpMain)
-})
-const server1 = net.createServer()
-server1.on('connection', (socket) => {
-      console.log('new connection')
-     
-
-      socket.on ('data', (data) => {
-           if(data.toString().includes(":")){
-            transcodingSocket.push(socket)
-            
-            
-            
-           }
-            
+      client1.on('end', () => {
+            console.log('disconnected from main server');
+            client2.end()
       })
-      
+
+};
+const MainServer = net.createServer();
+const SubServer = net.createServer();
+
+
+let infoSockets = {}
+let obsSockets = [];
+let tcpSockets = [];
+let commSockets = [];
+let MainSocketsIp = [];
+
+// connection emit when client is connected
+
+MainServer.on('connection', (socket) => {
+
+
+  console.log(socket.remoteAddress, 'connection');
+
+  if (MainSocketsIp.includes(socket.remoteAddress)) {
+    tcpSockets.push(socket);
+    console.log(tcpSockets.indexOf(socket), 'ffmpeg push');
+    infoSockets[socket.remoteAddress].push(socket)
+
+
+  } else {
+    obsSockets.push(socket);
+    console.log(socket.remoteAddress, 'obs push');
+    for (const sockets of commSockets) {
+      // sockets.write(`start:${socket.remoteAddress}`);
+    }
+  }
+
+  // data writing from obs-studio to transcoding
+
+  socket.on('data', (data) => {
+    if (obsSockets.includes(socket)) {
+      let i = obsSockets.indexOf(socket)
+      for (const ip of MainSocketsIp) {
+        const Arrayfind = infoSockets[ip]
+        if (Arrayfind[i] !== undefined) {
+          Arrayfind[i].write(data)
+        }
+      }
+
+    }
+  });
+
+  // emited when obs connection is closed
+
+  socket.once('close', () => {
+    if (obsSockets.includes(socket)) {
+      socketIndex = obsSockets.indexOf(socket);
+      obsSocketRemove(socketIndex);
+      tcpSocket(socketIndex);
+    }
+  });
+
+  socket.once('end', () => {
+    if (tcpSockets.includes(socket)) {
+      const index = tcpSockets.indexOf(socket);
+      tcpSockets.splice(index, 1);
+
+      for (const ip of MainSocketsIp) {
+        const Arrayfind = infoSockets[ip]
+        const index1 = Arrayfind.indexOf(socket);
+        Arrayfind.splice(index1, 1)
+
+      }
+
+      console.log('disconnected from transcodingTool');
+      // process.exit(0);
+    }
+  });
+});
+
+// removing obs socket from server
+const obsSocketRemove = (socketIndex) => {
+  console.log(
+    obsSockets[socketIndex].remoteAddress,
+    'obs connection has been closed',
+  );
+  //  obsSockets[socketIndex].destroy();
+  if (obsSockets[socketIndex] !== undefined) {
+    obsSockets[socketIndex].destroy();
+    obsSockets.splice(socketIndex, 1);
+  }
+};
+
+// removing FFMPEG socket from server
+
+const tcpSocket = (socketIndex) => {
+
+  if (tcpSockets[socketIndex] !== undefined) {
+    console.log(
+      tcpSockets[socketIndex].remoteAddress,
+      'ffmpeg connection has been closed',
+    );
+
+    for (const ip of MainSocketsIp) {
+      const Arrayfind = infoSockets[ip]
+
+      Arrayfind[socketIndex].destroy()
+      Arrayfind.splice(socketIndex, 1);
+    }
+    tcpSockets.splice(socketIndex, 1);
+
+    console.log(infoSockets)
+  }
+};
+
+MainServer.listen(9070, '0.0.0.0', () => {
+  console.log(`TCP Server listening on port 8000`);
+});
+
+
+SubServer.on('connection', (socket) => {
+  console.log(socket.remoteAddress, 'connection');
+  commSockets.push(socket)
+  MainSocketsIp.push(socket.remoteAddress)
+  infoSockets[socket.remoteAddress] = [];
+
 })
-
-server1.listen(config.ports.SubTcpCom, '0.0.0.0',()=>{
-      console.log('server is running on port', config.ports.SubTcpCom)
-}
-)
-
-
-
-
-
-
-
-
-
-const client = net.createConnection(config.ports.MainCom, config.main_tcp,()=>{
-      client.write(':8002')
+SubServer.listen(9080, '0.0.0.0', () => {
+  console.log(`Sub Server listening on port 9000`);
 })
-client.on('data', (data) => {})
