175b7edc90301076637b0bcc5ca40db942b99bb4
anandhu2205 <svananthu2@gmail.com>
2021-12-28 11:41:24 +0530

    video Transcoding phase/2

--- Video_Transcoding_Tool/index.js
+++ Video_Transcoding_Tool/index.js
@@ -12,113 +12,111 @@ const ffmpeg = require('./ffmpeg');
 const getData = require('./httpService');
 const { getStreamData, editStreamData } = require('./jsonService');
 const io = require('socket.io')(server, {
-      cors: {
-            orgin: '*',
-      },
+  cors: {
+    orgin: '*',
+  },
 });
 const networkInfo = require('./networkService')[0];
 app.get('/', (req, res) => {
-      res.json(`localhost:8085`);
+  res.json(`localhost:8085`);
 });
 app.get('/streamConfig', async (req, res) => {
-      const file = await getStreamData();
-      res.json(file);
+  const file = await getStreamData();
+  res.json(file);
 });
 
 app.get('/media/:channel/:stream/:quality', (req, res) => {
-      res.sendFile(
-            path.join(
-                  `/home/node/media/${req.params.channel}/${req.params.stream}/${req.params.quality}`,
-            ),
-      );
-      // res.sendFile(`/home/user/Desktop/storage/${req.params.stream}/${req.params.quality}`);
+  res.sendFile(
+    path.join(
+      `/home/node/media/${req.params.channel}/${req.params.stream}/${req.params.quality}`,
+    ),
+  );
+  // res.sendFile(`/home/user/Desktop/storage/${req.params.stream}/${req.params.quality}`);
 });
 server.listen(port, () => {
-      console.log('server running on ', port);
+  console.log('server running on ', port);
 });
 
 let streamTotal = [];
 const mediaPath = path.join('./media');
 
-io.on('connection', async (socket) => {
-      await getStreamData();
-      // const data = require('./streamConfig.json');
-      socket.emit('playerLoaded', 'connected');
+io.on('connection', (socket) => {
+  console.log('new Connection');
+  // const data = require('./streamConfig.json');
+  socket.emit('playerLoaded', 'connected');
 });
 
 //  io.sockets.emit('stream', streamTotal);
 const tcpServerUrl = new URL(config.tcp_Server_address);
 var ffmpegConnection = net.createConnection(
-      {
-            port: config.tcp_communication_port,
-            host: tcpServerUrl.hostname,
-      },
-      () => {
-            console.log('connected to TCP Server');
-            // ffmpegConnection.write(`${networkInfo}:${tcpServerUrl.port}`);
-            ffmpegConnection.once('end', function () {
-                  console.log('disconnected from TCP Server');
-                  // process.exit(0);
-            });
-      },
+  {
+    port: config.tcp_communication_port,
+    host: tcpServerUrl.hostname,
+  },
+  () => {
+    console.log('connected to TCP Server');
+    // ffmpegConnection.write(`${networkInfo}:${tcpServerUrl.port}`);
+    ffmpegConnection.once('end', function () {
+      console.log('disconnected from TCP Server');
+      // process.exit(0);
+    });
+  },
 );
 
 ffmpegConnection.on('data', async (data) => {
-      console.log(data.toString(),'check');
+  console.log(data.toString(), 'check');
 
-      const videoConfig = await getStreamData();
+  const videoConfig = await getStreamData();
+  console.log(videoConfig);
+  //   const json = videoConfig;
 
-      const json = JSON.parse(videoConfig);
+  const getVideoData = videoConfig.data.find(
+    (obj) => obj.streamKey === data.toString(),
+  );
+  if (getVideoData !== undefined) {
+    const getStreamName = getVideoData.streams.find(
+      (name) => name.live === undefined,
+    );
+    if (getStreamName) {
+      let streamId = getStreamName.streamingName;
+      let channelName = getVideoData.channelName;
+      let streamName = streamId;
+      // let id = parseInt(streamId.match(/\d+/)[0]);
 
-      const getVideoData = json.data.find(
-            (obj) => obj.streamKey === data.toString(),
-      );
-      if (getVideoData !== undefined) {
-            const getStreamName = getVideoData.streams.find(
-                  (name) => name.live === undefined,
-            );
-            if (getStreamName) {
-                  let streamId = getStreamName.streamingName;
-                  let channelName = getVideoData.channelName;
-                  let streamName = streamId;
-                  // // let id = parseInt(streamId.match(/\d+/)[0]);
-                 
-                  ffmpeg(streamName, channelName);
-                  socketEmit(streamName, channelName);
-            }
-      } else {
-            console.log('no stream key found ');
-      }
+      ffmpeg(streamName, channelName);
+      socketEmit(streamName, channelName);
+    }
+  } else {
+    console.log('no stream key found ');
+  }
 });
 const socketEmit = (streamName, ChannelName) => {
-      const intervalObj = setInterval(async () => {
-            const file = `/home/node/media/${ChannelName}/${streamName}/master.m3u8`;
-            // const file = `/home/user/Desktop/storage/${streamName}/master.m3u8`;
-            try {
-                  const fileExists = await fs.pathExists(file);
-                  const stats = await fs.stat(`${file}`);
+  const intervalObj = setInterval(async () => {
+    const file = `/home/node/media/${ChannelName}/${streamName}/master.m3u8`;
+    // const file = `/home/user/Desktop/storage/${streamName}/master.m3u8`;
+    try {
+      const fileExists = await fs.pathExists(file);
+      const stats = await fs.stat(`${file}`);
 
+      // let streamCreated;
+      if (fileExists) {
+        const data = {
+          channel: ChannelName,
+          name: streamName,
+          time: Date.now(),
+          live: true,
+        };
+        console.log(data);
+        editStreamData('createTimeFrame', data);
 
-                  // let streamCreated;
-                  if (fileExists) {
-                        const data = {
-                              channel: ChannelName,
-                              name: streamName,
-                              time: Date.now(),
-                              live: true,
-                        };
-                        console.log(data);
-                        editStreamData('createTimeFrame', data);
-
-                        io.sockets.emit('onStreamAdd', {
-                              channelName: ChannelName,
-                              streamName: streamName,
-                        });
-                        clearInterval(intervalObj);
-                   
-                  }
-            } catch (e) {
-                  console.log(e, 'error');
-            }
-      }, 2000);
+        io.sockets.emit('onStreamAdd', {
+          channelName: ChannelName,
+          streamName: streamName,
+        });
+        clearInterval(intervalObj);
+      }
+    } catch (e) {
+      console.log(e, 'error');
+    }
+  }, 2000);
 };
--- Video_Transcoding_Tool/jsonService.js
+++ Video_Transcoding_Tool/jsonService.js
@@ -1,78 +1,98 @@
 const fs = require('fs-extra');
-const {create,globSource} = require('ipfs-http-client');
+const path = require('path');
+const { create, globSource } = require('ipfs-http-client');
 const config = require('./settings/config.json');
-const ipfs = create( `http://${config.ipfs.host}:${config.ipfs.port}`);
+const ipfs = create(`http://${config.ipfs.host}:${config.ipfs.port}`);
 const filePath = config.ipfs.streamConfigPathIpfs;
+const fileName = filePath.split('/')[1];
+const axios = require('axios');
+const { exec } = require('child_process');
 
-
+let failedCount = 0;
 const getStreamData = async () => {
-  for await(const file of ipfs.files.read(filePath)){
-       const json = JSON.parse(file.toString());
-        fs.writeJSON('./streamVideoConfig.json', json);
-      return file.toString()
-};
+  const response = await axios.post(
+    `http://${config.ipfs.host}:${config.ipfs.port}/api/v0/files/read?arg=${filePath}`,
+  );
 
- //await fs.writeFile('./streamConfig.json', data);
-      // return data
+  console.log(response.data);
+  await fs.writeJSON(`./${fileName}`, response.data);
+  return response.data;
 };
 
 const createTime = async (data) => {
-      const oldJson = await getStreamData()
-      console.log(oldJson);
-       const newJson = JSON.parse(oldJson)
+  const oldJson = fs.readFileSync(`./${fileName}`, 'utf8');
+  console.log(oldJson);
+  const newJson = JSON.parse(oldJson);
 
-      let streamsArr =
-      newJson.data[
-            newJson.data.findIndex(
-                        (obj) => obj.channelName === data.channel,
-                  )
-            ].streams;
-            console.log(streamsArr);
+  let streamsArr =
+    newJson.data[
+      newJson.data.findIndex((obj) => obj.channelName === data.channel)
+    ].streams;
+  console.log(streamsArr);
 
-      let streamObj =
-            streamsArr[
-                  streamsArr.findIndex((obj) => obj.streamingName === data.name)
-            ];
-      streamObj.streamingName = data.name;
-      streamObj.started_timeStamp = data.time;
-      streamObj["live"] = data.live;
-     fs.writeJSON('./streamVideoConfig.json', newJson);
-       await ipfs.files.rm(filePath);
-     await ipfs.files.write(filePath, Buffer.from(JSON.stringify(newJson)), { create: true });
-      
+  let streamObj =
+    streamsArr[streamsArr.findIndex((obj) => obj.streamingName === data.name)];
+  streamObj.streamingName = data.name;
+  streamObj.started_timeStamp = data.time;
+  streamObj['live'] = data.live;
+  await fs.writeJSON(`./${fileName}`, newJson);
+  await ipfs.files.rm(filePath);
+  const child = exec(
+    `curl -X POST -F file=@'./${fileName}' "http://${config.ipfs.host}:${config.ipfs.port}/api/v0/files/write?arg=${filePath}&create=true"`,
+  );
+  child.stdout.on('data', (data, err) => {
+    console.log(err);
+    console.log(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.log(data.toString());
+  });
+  // await ipfs.files.rm(filePath);
+  // await ipfs.files.write(filePath, Buffer.from(JSON.stringify(newJson)), {
+  //       create: true,
+  // });
 };
 
 const live = async (data) => {
-      const oldJson = await getStreamData()
-      const newJson = JSON.parse(oldJson)
-      let streamsArr =
-            newJson.data[
-                  newJson.data.findIndex(
-                        (obj) => obj.channelName === data.channel,
-                  )
-            ].streams;
-      console.log(streamsArr);
-      let streamObj =
-            streamsArr[
-                  streamsArr.findIndex((obj) => obj.streamingName === data.name)
-            ];
-      console.log(data);
-      streamObj.live = data.live;
-      streamObj.ended_timeStamp = data.endedtime;
-      fs.writeJson('./streamVideoConfig.json', newJson);
-      await ipfs.files.rm(filePath);
-     await ipfs.files.write(filePath, Buffer.from(JSON.stringify(newJson)), { create: true });
-      
+  const oldJson = fs.readFileSync(`./${fileName}`, 'utf8');
+
+  const newJson = JSON.parse(oldJson);
+  let streamsArr =
+    newJson.data[
+      newJson.data.findIndex((obj) => obj.channelName === data.channel)
+    ].streams;
+  console.log(streamsArr);
+  let streamObj =
+    streamsArr[streamsArr.findIndex((obj) => obj.streamingName === data.name)];
+  console.log(data);
+  streamObj.live = data.live;
+  streamObj.ended_timeStamp = data.endedtime;
+  await fs.writeJson(`./${fileName}`, newJson);
+  await ipfs.files.rm(filePath);
+  const child = exec(
+    `curl -X POST -F file=@'./${fileName}' "http://${config.ipfs.host}:${config.ipfs.port}/api/v0/files/write?arg=${filePath}&create=true"`,
+  );
+  child.stdout.on('data', (data, err) => {
+    console.log(err);
+    console.log(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.log(data.toString());
+  });
+  // await ipfs.files.rm(filePath);
+  // await ipfs.files.write(filePath, Buffer.from(JSON.stringify(newJson)), {
+  //       create: true,
+  // });
 };
 
-const editStreamData =  (action, data) => {
- 
-     if(action ==='createTimeFrame'){
-       createTime(data);
-     }else{
-            live(data);
-     }
-     
+const editStreamData = (action, data) => {
+  if (action === 'createTimeFrame') {
+    createTime(data);
+  } else {
+    live(data);
+  }
 };
 
 module.exports = { getStreamData, editStreamData };
